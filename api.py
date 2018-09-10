e4613afc7bd9316eb1a3fc5d1d91e5ba2797379b
rhcowell <rh.cowell@gmail.com>
2018-09-10 17:25:59 -0400

    initial commit

---
+++ api.py
@@ -0,0 +1,60 @@
+import tweepy
+import json
+import time
+
+with open('api_keys.json') as f:
+    keys = json.load(f)
+
+auth = tweepy.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
+auth.set_access_token(keys['access_token'], keys['access_token_secret'])
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+def limit_handled(cursor):
+    """
+    handles the rate limit of Twitter API
+    waits until rate limit time expires (15 min)
+    :param cursor: Cursor object from Tweepy (includes API method and its params)
+    :return: yields the next page from the specified API method
+    """
+
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            print("WAITING...")
+            time.sleep(15 * 60)
+
+
+def get_follower_ids(source_id):
+    """
+    gets Twitter user's followers
+
+    :param source_id: the Twitter id of the source user
+    :return: list of Twitter ids - followers of source user
+    """
+
+    num = 0
+    followers = []
+    for follower in limit_handled(tweepy.Cursor(api.followers_ids, id=source_id).items()):
+        num += 1
+        followers.append(follower)
+    print "User " + str(source_id) + " has " + str(num) + "followers"
+    return followers
+
+
+def get_following_ids(source_id):
+    """
+    gets users who Twitter user follows (following/friends)
+
+    :param source_id: the Twitter id of the source user
+    :return: list of Twitter ids - source user following list
+    """
+
+    num = 0
+    following = []
+    for following_user in limit_handled(tweepy.Cursor(api.friends_ids, id=source_id).items()):
+        num += 1
+        following.append(following_user)
+    print "User " + str(source_id) + " is following " + str(num)
+    return following
